4596c111cab236696e28c6a03eda124663866d4c
Samuel Fernandes <samuelfernandesotaviano@gmail.com>
2020-03-25 22:22:40 -0300

    Back-end concluido

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,23 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(request, response) {
-        const incidents = await connection('incidents').select('*');
+        const { page = 1 } = request.query;
+
+        const [count] = await connection('incidents').count();
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select(['incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ]);
+
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
@@ -23,9 +39,9 @@ module.exports = {
         const ong_id = request.headers.authorization;
 
         const incident = await connection('incidents')
-        .where('id', id)
-        .select('ong_id')
-        .first();
+            .where('id', id)
+            .select('ong_id')
+            .first();
 
         if (incident.ong_id !== ong_id) {
             return response.status(401).json({ error: 'Operacao nao permitida.' });
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
+const SessionController = require('./controllers/SessionController');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
+routes.post('/sessions', SessionController.create);
+
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
