087148bfb56151968a02e6200c196e870783e56c
Samuel Fernandes <samuelfernandesotaviano@gmail.com>
2020-03-26 23:06:20 -0300

    Front-end do app

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
+import logoImg from '../../assets/logo.svg';
 import herosImage from '../../assets/heroes.png';
+import { FiLogIn } from 'react-icons/fi';
 
 export default function Logon() {
     return (
         <div className="logon-container">
-            <section className="from"></section>
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero" />
+
+                <form>
+                    <h1>Faca seu logon</h1>
+
+                    <input placeholder="Sua ID"/>
+                    <button className="button" type="submit">Entrar</button>
+
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={24} color="#e02041"/>
+                        Nao tenho cadastro
+                    </Link>
+                </form>
+            </section>
 
             <img src={herosImage} alt="Heroes" />
         </div>
